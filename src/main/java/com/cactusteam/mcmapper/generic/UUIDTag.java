0ef11936593e696bcdd54fc3ac842e2df3adcb58
Limo <mail@vogel-julian.de>
2024-08-16 00:14:35 +0200

    Add UUIDTag

---
+++ src/main/java/com/cactusteam/mcmapper/generic/UUIDTag.java
@@ -0,0 +1,13 @@
+package com.cactusteam.mcmapper.generic;
+
+import com.cactusteam.mcmapper.tag.ArrayTag;
+import com.cactusteam.mcmapper.tag.TagType;
+
+public class UUIDTag extends ArrayTag<Integer> {
+
+    public UUIDTag(String id) {
+        super(id, TagType.CUSTOM);
+        for (int i = 0; i < 4; i++)
+            entries.add(0);
+    }
+}
